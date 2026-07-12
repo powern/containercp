bcd20a0b0a42ede861f45a1bd318aae5d3a15080
Serhii Kryvsha <powern76@gmail.com>
2026-07-12 18:49:25 +0300

    fix(webmail): add /snappymail/ proxy location for static assets
    SnappyMail HTML references assets at /snappymail/v/... (absolute paths).
    Added second location /snappymail/ that proxies to the same backend
    so static files are served correctly.

--- libs/proxy/ProxyConfigBuilder.cpp
+++ libs/proxy/ProxyConfigBuilder.cpp
@@ -14,6 +14,13 @@ std::string ProxyConfigBuilder::build(const Params& params) const {
             "        proxy_set_header X-Real-IP $remote_addr;\n"
             "        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n"
             "        proxy_set_header X-Forwarded-Proto $scheme;\n"
+            "    }\n"
+            "    location /snappymail/ {\n"
+            "        proxy_pass http://" + params.webmail_upstream + ";\n"
+            "        proxy_set_header Host $host;\n"
+            "        proxy_set_header X-Real-IP $remote_addr;\n"
+            "        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n"
+            "        proxy_set_header X-Forwarded-Proto $scheme;\n"
             "    }\n";
     }
 
